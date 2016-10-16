eb4206a7eb5f5e760f35bbbed89f136613d587eb
Wilson Júnior <wilsonpjunior@gmail.com>
2016-10-16 18:35:49 -0200

    Add HTTP main interface

--- handler/handler.go
+++ handler/handler.go
@@ -1,8 +1,13 @@
 package handler
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
+	"net/url"
+	"strings"
+
+	"github.com/wpjunior/hyperbigbang/fetch"
+	"gopkg.in/vmihailenco/msgpack.v2"
 )
 
 type Handler struct{}
@@ -13,5 +18,49 @@ func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
-	fmt.Fprintf(w, "Hello: %s", url)
+
+	accept := r.Header.Get("accept")
+	if accept == "" {
+		accept = "application/json"
+	} else {
+		r.Header.Del("accept")
+	}
+
+	includes := extractRelIncludes(r.URL)
+	result, err := fetch.URL(url, r.Header, includes)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if accept == "application/msgpack" {
+		writeMsgPackResponse(w, result)
+	} else {
+		writeJSONResponse(w, result)
+	}
+}
+
+func writeJSONResponse(w http.ResponseWriter, result interface{}) {
+	w.Header().Add("content-type", "application/json")
+	err := json.NewEncoder(w).Encode(result)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+}
+
+func writeMsgPackResponse(w http.ResponseWriter, result interface{}) {
+	w.Header().Add("content-type", "application/msgpack")
+	err := msgpack.NewEncoder(w).Encode(result)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+}
+
+func extractRelIncludes(u *url.URL) []string {
+	raw := u.Query().Get("include")
+	return strings.Split(raw, ",")
 }
